a92204fb5919b908d8cab22bf7dea972754f7183
salt <boluuuu@gmail.com>
2018-01-21 04:00:16 +0100

    -add splitSection component

---
+++ client/component/splitSection/splitSection.js
@@ -0,0 +1,67 @@
+(function() {
+    "use strict";
+
+    defineComp("split-section", function (global, $element) {
+        let sectionID = $element.attr("id");
+        let _compNames = $element.attr("defaults");
+
+        if (!sectionID || sectionID === "") {
+            throw new Error("section need id!");
+        }
+
+        // if (_compNames) {
+        //     //load default
+        //
+        //     let $comp = $("<" + _compName + ">");
+        //     $element.append($comp);
+        // }
+        let dragged;
+        $(".handle", $element).mousedown(function(e) {
+            console.log("drag", e);
+            dragged = e;
+            // e.target.style.opacity = .5;
+        });
+        $(document).mousemove(function(e) {
+            if (dragged) {
+                console.log(dragged, "mousemove", e);
+
+            }
+            // if ()
+            // console.log("mouseMove");
+            // dragged = e.target;
+            // e.target.style.opacity = .5;
+        });
+        $(document).mouseup(function(e) {
+            console.log("mouseup", e);
+            dragged = null;
+            // console.log("mouseUp");
+            // dragged = e.target;
+            // e.target.style.opacity = .5;
+        });
+
+        this.onLoad = () => {};
+        this.load = (compName) => {
+            //@todo unload current comp
+            _compName = compName;
+            $element.empty();
+            let $comp = $("<" + compName + ">");
+            $element.append($comp);
+            global.loadComponent($comp, (ctx) => {
+
+            });
+        };
+        this.disableSection = () => {
+            //
+        }
+        // this.componentType = () => {
+        //     return
+        // };
+        // this.getComponent = () => {
+        //     return
+        // };
+
+    }, {
+        templatePath: "/jsfair/component/splitSection/splitSection.html"
+    });
+})();
+
