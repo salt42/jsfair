b2ed3738995f3f6154dd001159068d177e4447e6
salt <boluuuu@gmail.com>
2018-01-22 19:02:30 +0100

    -split section html changes

--- client/component/splitSection/splitSection.js
+++ client/component/splitSection/splitSection.js
@@ -3,52 +3,60 @@
 
     defineComp("split-section", function (global, $element) {
         let sectionID = $element.attr("id");
-        let _compNames = $element.attr("defaults");
+        let _compNames = $element.attr("default");
+
+        let $compOne;
+        let $compTwo;
 
         if (!sectionID || sectionID === "") {
             throw new Error("section need id!");
         }
 
-        // if (_compNames) {
-        //     //load default
-        //
-        //     let $comp = $("<" + _compName + ">");
-        //     $element.append($comp);
-        // }
-        let dragged;
-        $(".handle", $element).mousedown(function(e) {
-            console.log("drag", e);
-            dragged = e;
-            // e.target.style.opacity = .5;
-        });
-        $(document).mousemove(function(e) {
-            if (dragged) {
-                console.log(dragged, "mousemove", e);
+        if (_compNames) {
+            //load default
+            let comps = _compNames.split(",");
+            $compOne = $("<" + comps[0] + ">");
+            $(".item.one", $element).append($compOne);
+            $compTwo = $("<" + comps[1] + ">");
+            $(".item.two", $element).append($compTwo);
+        }
 
-            }
-            // if ()
-            // console.log("mouseMove");
-            // dragged = e.target;
-            // e.target.style.opacity = .5;
-        });
-        $(document).mouseup(function(e) {
-            console.log("mouseup", e);
-            dragged = null;
-            // console.log("mouseUp");
-            // dragged = e.target;
-            // e.target.style.opacity = .5;
-        });
+
+
+        // let dragged;
+        // $(".handle", $element).mousedown(function(e) {
+        //     console.log("drag", e);
+        //     dragged = e;
+        //     // e.target.style.opacity = .5;
+        // });
+        // $(document).mousemove(function(e) {
+        //     if (dragged) {
+        //         console.log(dragged, "mousemove", e);
+        //
+        //     }
+        //     // if ()
+        //     // console.log("mouseMove");
+        //     // dragged = e.target;
+        //     // e.target.style.opacity = .5;
+        // });
+        // $(document).mouseup(function(e) {
+        //     console.log("mouseup", e);
+        //     dragged = null;
+        //     // console.log("mouseUp");
+        //     // dragged = e.target;
+        //     // e.target.style.opacity = .5;
+        // });
 
         this.onLoad = () => {};
         this.load = (compName) => {
-            //@todo unload current comp
-            _compName = compName;
-            $element.empty();
-            let $comp = $("<" + compName + ">");
-            $element.append($comp);
-            global.loadComponent($comp, (ctx) => {
-
-            });
+            // //@todo unload current comp
+            // _compName = compName;
+            // $element.empty();
+            // let $comp = $("<" + compName + ">");
+            // $element.append($comp);
+            // global.loadComponent($comp, (ctx) => {
+            //
+            // });
         };
         this.disableSection = () => {
             //
