0f63ae994586e27c73414a70a6dfed59d649c03a
salt <boluuuu@gmail.com>
2018-01-24 17:17:42 +0100

    add devShell

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ module.paths.push(__dirname + "/server");
 process.env.NODE_PATH = __dirname + "/server";
 require('module').Module._initPaths();
 
-let fs      = require("fs");
-let colors  = require('colors');
-let log     = require('jsfair/log')('main');
-let hook    = require('./server/hook');
+const colors  = require('colors');
+const fs      = require("fs");
+const log     = require('jsfair/log')('main');
+const hook    = require('./server/hook');
 
+let rootPath = "";
 let hookProxyHandler = {
     get: function(target, name) {
         return function(...args) {
@@ -20,42 +21,78 @@ let hookProxyHandler = {
         }
     }
 };
+global.hookIn = new Proxy({}, hookProxyHandler);
+global.ROOT_PATH = fs.realpathSync(rootPath);
+global.jsfairPath = fs.realpathSync(__dirname);
 
-module.exports = function(rootPath) {
-    global.hookIn = new Proxy({}, hookProxyHandler);
-    global.ROOT_PATH = fs.realpathSync(rootPath);
-    global.jsfairPath = fs.realpathSync(__dirname);
+//handle arguments
+function helpPage(err) {
+    console.log("Error: %s".red, err);
+    console.log("Usage: node main.js --root ../pathToProjectFolder [arguments]");
+}
+
+if (process.argv.indexOf('--root') > -1) {
+    rootPath = process.argv[process.argv.indexOf('--root') + 1];
+    if (rootPath === "") {
+        helpPage("can't start without root path!");
+        return;
+    }
+} else {
+    helpPage("can't start without root path!");
+    return;
+}
+
+const conf = require('jsfair/config')(rootPath + "/conf.json");
+//dev mode
+if (process.argv.indexOf('--dev') > -1) {
+    //setup dev websocket server
+
+    //add devBridge.js to preScripts config
+    // conf
+    process.on('message', message => {
+        console.log('message from parent:', message);
+        if (!message.hasOwnProperty("com")) return;
+        switch(message.com) {
+            case "refreshClients":
+                //@todo rebuild -> send reload command to client
+                //@todo damit des funtz muss noch ein devClientBridge basteln die eine socket verbindung mit einem extra ws server aufbaut
+                //@todo -> und dann können wir dadrüber des reload command senden und die seite automatisch reloaden
+
+                break;
+        }
+    });
+}
+
+try {
+    let db = require("jsfair/database");
+    let express = require("./server/express");
+    log("Start %s Server", conf.appName);
     try {
-        let conf = require('jsfair/config')(rootPath + "/conf.json");
-        let db = require("jsfair/database");
-        let express = require("./server/express");
-        log("Start %s Server", conf.appName);
-        try {
-            for (let x = 0; x < conf.server.modulePaths.length; x++) {
-                let loadPath = fs.realpathSync(rootPath + "/" + conf.server.modulePaths[x]);
-                let dir = fs.readdirSync(loadPath);
-                for (let i = 0; i < dir.length; i++) {
-                    let error = false;
-                    try {
-                        require(loadPath + "/" + dir[i]);
-                    } catch(e) {
-                        error = true;
-                        log("Error in module: %s (%s)".red, dir[i].yellow, loadPath);
-                        // log("%s",  e.message.red);
-                        log("%s",  e.stack.red);
-                    }
-                    if (!error) {
-                        log("Module loaded: %s", dir[i].green);
-                    }
+        //@todo if conf.client.defaultComponent.length > 0 then load this modules
+        for (let x = 0; x < conf.server.modulePaths.length; x++) {
+            let loadPath = fs.realpathSync(rootPath + "/" + conf.server.modulePaths[x]);
+            let dir = fs.readdirSync(loadPath);
+            for (let i = 0; i < dir.length; i++) {
+                let error = false;
+                try {
+                    require(loadPath + "/" + dir[i]);
+                } catch(e) {
+                    error = true;
+                    log("Error in module: %s (%s)".red, dir[i].yellow, loadPath);
+                    // log("%s",  e.message.red);
+                    log("%s",  e.stack.red);
+                }
+                if (!error) {
+                    log("Module loaded: %s", dir[i].green);
                 }
             }
-        } catch (e) {
-            log(e.message.red);
-            console.log(e);
         }
-        db.init();
-        express.init();
     } catch (e) {
+        log(e.message.red);
         console.log(e);
     }
-};
+    db.init();
+    express.init();
+} catch (e) {
+    console.log(e);
+}
