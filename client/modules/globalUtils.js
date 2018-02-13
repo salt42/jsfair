16a39dbc6378a7b1dd81563d750f74962ac5d0f1
Fry <Fry.LMU@gmx.de>
2018-02-13 01:18:14 +0100

    opened hideOnOuterClick() for mor general reasons

--- client/modules/globalUtils.js
+++ client/modules/globalUtils.js
@@ -2,18 +2,45 @@
     "use strict";
 
 
-    $.fn.hidesOnOuterClick = function(hideSelector) {
+    $.fn.hidesOnOuterClick = function(excludeSelector) {
         let self = this;
-        document.onclick = function(e){
-            hideSelector = (!!hideSelector) ? $(hideSelector[0]) : self;
-            console.log(!hideSelector.is(e.target));
-            console.log(hideSelector.has(e.target).length === 0);
-            console.log('###');
+        let select = (!!excludeSelector) ? "1" : "0";
+        if (excludeSelector) {
+            if (excludeSelector.has(self).length !== 0) select += "1";
+            else if (self.has(excludeSelector).length !== 0) select += "2";
+            else select += "0";
+        }
 
-            // if the target of the click isn't the container nor a descendant of the container
-            if (!hideSelector.is(e.target) && hideSelector.has(e.target).length === 0){
-                self.addClass("hidden");
-                self.removeOuterClick();
+        document.onclick = function(e){
+            switch(select){
+                case "0":
+                    // if the target of the click isn't the container nor a descendant of the container
+                    if (!self.is(e.target) && self.has(e.target).length === 0){
+                        self.addClass("hidden");
+                        self.removeOuterClick();
+                    }
+                    break;
+                case "10":
+                    // if the target of the click isn't a descendant of the container nor the excluded selector
+                    if (self.has(e.target).length === 0 && !excludeSelector.is(e.target)){
+                        self.addClass("hidden");
+                        self.removeOuterClick();
+                    }
+                    break;
+                case "11":
+                    // self is in hideSelector and if the target of the click isn't the container nor a descendant of the container
+                    if (!excludeSelector.is(e.target) && excludeSelector.has(e.target).length === 0){
+                        self.addClass("hidden");
+                        self.removeOuterClick();
+                    }
+                    break;
+                case "12":
+                    // hideSelector is in self and if the target of the click isn't the container nor a descendant of the container
+                    if (!excludeSelector.is(e.target) && self.has(e.target).length === 0){
+                        self.addClass("hidden");
+                        self.removeOuterClick();
+                    }
+                    break;
             }
         };
 
