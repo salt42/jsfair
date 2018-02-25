322aa940a28c107bd7b8e87a63ba8a2873b5be12
Fry <Fry.LMU@gmx.de>
2018-02-25 11:41:00 +0100

    moved Treatment view to js added prepend Template function to jquery

--- client/modules/templateUtils.js
+++ client/modules/templateUtils.js
@@ -22,5 +22,30 @@
     //     //place data in template
     // });
     /*endregion*/
+
+    $.fn.prependTemplate = function(template, list, fn) {
+        template = $(template)[0];
+        if (!template) throw new Error("template not '%s' found!", template);
+        let rootFragment = document.createDocumentFragment();
+        for (let i = 0; i < list.length; i++) {
+            if(list[i].skip && list[i].skip === true) continue;
+            let subFrag = template.content.cloneNode(true);
+            if (typeof fn === "function") fn(subFrag, list[i]);
+            rootFragment.prepend(subFrag);
+        }
+        let $ele = document.importNode(rootFragment, true);
+        this.prepend($ele);
+        return this;
+    };
+
+    /* region sample */
+    // let listData = [{name:"ich"},{name:"du"},{name:"er"}];
+    //
+    // $("ul").prependTemplate($("template"), listData, function(fragment, value) {
+    //     //place data in template
+    // });
+    /*endregion*/
+
+
 }(jQuery));
 
